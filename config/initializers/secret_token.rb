b5ef36da73a5a1995bab640ae1287c06e0cdbdb1
David Chen <mvjome@gmail.com>
2014-01-13 16:47:53 +0800

    git init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LetPaymentFly::Application.config.secret_key_base = '2c51e775e7fed4bf19c265b117f7d46731cb2709a2b41c99cd83361412724119f47540badeffa2617fe16033e5e77a31d3a258c224a303bb2f09d938d8d9782e'
