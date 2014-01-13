0ff4b8e542c02c668dbd4b3d46a868dc4acb5c16
David Chen <mvjome@gmail.com>
2014-01-13 21:04:39 +0800

    Add reque related

--- lib/resque/payment.rb
+++ lib/resque/payment.rb
@@ -4,8 +4,13 @@ module Resque
   class Payment
     @queue = self.name
 
-    def self.perform item_id
-      sleep 2.5
+    def self.perform payment_id
+      sleep 3
+      puts "process Payment[#{payment_id}]"
+
+      @order = Order.find_or_create_by_payment_id(payment_id)
+      @order.status = !(rand(3) % 3).zero? # only two-thirds opportunity of success
+      @order.save
     end
 
   end
